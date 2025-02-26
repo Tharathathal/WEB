79ff001d390774eccd5515d7ba89c23eedc062ee
Tharathathal <thaliacharton@free.fr>
2025-02-26 15:08:25 +0100

    starter

---
+++ projet-web/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    
+    const handleClick = () => setClick(!click);
+
+    return (
+    <>
+        <nav class="navbar">
+            <div class="navbar-container">
+                <Link to='/' className='navbar-logo'>
+                    TRVL
+                    <i class='fab fa-typo3' />
+                </Link>
+                <div className='menu-icon' onClick={handleClick}>
+                    <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                </div>
+            </div>
+        </nav> 
+    </>
+  )
+}
+
+export default Navbar
