0658d3b93e45b5d3e22754fd297069630f874f75
Tharathathal <thaliacharton@free.fr>
2025-04-13 21:03:49 +0200

    fix(css): modified some rendering

--- projet-web/src/components/pages/Home.js
+++ projet-web/src/components/pages/Home.js
@@ -9,13 +9,13 @@ function Home() {
         <h1>Jeu de société en ligne</h1>
         <h2>Quarto</h2>
         <div class="presentation">
-          <img src="/images/quarto_box.webp" alt="Boîte du jeu de société Quarto" class="game-box" />
+          <img src="/images/quarto_box.webp" alt="Boîte du jeu de société Quarto" class="game-box" width={300} height={300}/>
           <p>
             Jeu de société au tour par tour, combinatoire (réflexion, décision) et à information complète et parfaite <br />
             Se joue à 2 joueurs <br />
             À partir de 8 ans <br />
           </p>
-          <img src="/images/quarto.webp" alt="Un exemple de partie en cours" class="game-example" />
+          <img src="/images/quarto.webp" alt="Un exemple de partie en cours" class="game-example" width={300} height={266}/>
         </div>
       </div>
     </>
